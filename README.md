23958f0820eced13d7e2797ec91cdc004ea613e6
damithc <1673303+damithc@users.noreply.github.com>
2025-09-17 18:36:02 +0800

    Add initial files

---
+++ README.md
@@ -0,0 +1,8 @@
+# Smoke-Testing iP/tP JAR files
+
+## General steps:
+
+1. Set up the required JRE/JDK and include them in your PATH
+1. Create a folder 'jars' in the same directory as the `<os>.py` script
+1. Place all the jars to be tested in that folder
+1. Run the script and wait until it generates a csv file
---
+++ linux.py
@@ -0,0 +1,40 @@
+import csv
+import os
+import shutil
+import subprocess
+
+JAVA_PATH = "java"
+
+table = []
+
+for root, dirs, files in os.walk("jars"):
+    for jarname in files:
+        if jarname.endswith(".jar"):    #confirm
+            print(jarname)
+            jarpath = os.path.join(root, jarname)
+            if not os.path.exists("jar_test"):
+                os.makedirs("jar_test")
+            shutil.copy2(jarpath,"jar_test")
+            os.chdir("jar_test")
+            isOk = False
+            jarproc = subprocess.Popen([JAVA_PATH, "-jar", jarname],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE, shell=False)
+            try:
+                poll = jarproc.wait(1)
+            except subprocess.TimeoutExpired:
+                print("Ok")
+                jarproc.kill()
+                isOk = True
+            if isOk:
+                err = ""
+            else:
+                #rerun and log the error
+                jarproc = subprocess.Popen([JAVA_PATH, "-jar", jarname],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE, shell=False)
+                err = jarproc.communicate()[1]
+                jarproc.kill()  #shouldn't be necessary
+            table.append([str(jarname), str(err)])
+            os.chdir("..")
+            shutil.rmtree("jar_test")
+
+with open("linux.csv","w",newline="") as f:
+    writer = csv.writer(f)
+    writer.writerows(table)
